6e68f3ec4d79c4b751704dd7e0eb1c9e0c240a05
Monica Grandy <monica.grandy@gmail.com>
2016-04-25 09:24:59 -0700

    adds auth to config file

--- db/db.js
+++ db/db.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var fs = require('fs')
 var config = require('./config.js')
 
 var con = mysql.createConnection({
@@ -13,13 +14,44 @@ con.connect(function(err){
     console.log('Error connecting to Db');
     return;
   }
-  console.log('Connection established');
+  console.log('line 17: Connection established');
+//   fs.readFile(__dirname + '/schema.sql', 'utf-8', function(err, data){
+//   console.log("line 19")  
+//   var commands = data.split(";");
+//   commands.pop();
+//   commands.forEach(function(command){
+//     con.query(command, function(err, results){
+//       if (err){
+//         console.error(err);
+//       }
+//       //console.log("line 27", results) 
+//     })
+//   })
+// })
+
+// con.query('INSERT INTO CLIENT CONTACT (name, email, rating) VALUES (?, ?, ?)', ['Monica', 'monica.grandy@gmail.com', '5'], function(err, result) {
+//       if (err){
+//         throw err
+//       } 
+//       else {
+//         console.log(result)
+//       }
+//   }); 
 });
 
-con.end(function(err) {
-  // The connection is terminated gracefully
-  // Ensures all previously enqueued queries are still
-  // before sending a COM_QUIT packet to the MySQL server.
-});
+
+
+// setInterval(function () { con.query('SELECT 1') }, 5000)
+
+// con.end(function(err) {
+//   // The connection is terminated gracefully
+//   // Ensures all previously enqueued queries are still
+//   // before sending a COM_QUIT packet to the MySQL server.
+// });
 
 module.exports = con
+
+
+
+
+
